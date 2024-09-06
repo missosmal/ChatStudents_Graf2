c44df6dfdb3e250927e8fa921634afa5e0adc069
Muxaaaa <20RWplay@gmail.com>
2024-09-06 09:49:15 +0500

    Разработанные контексты данных.Graf

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,15 @@
+﻿using ChatStudents_Graf.Classes.Common;
+using ChatStudents_Graf.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents_Graf.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+        public MessagesContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,15 @@
+﻿using ChatStudents_Graf.Classes.Common;
+using ChatStudents_Graf.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents_Graf.Classes
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
