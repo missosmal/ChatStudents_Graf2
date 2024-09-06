3ed70f62037802d4ebbc72075dd109a76f0ce179
Muxaaaa <20RWplay@gmail.com>
2024-09-06 09:56:39 +0500

    Разработанный пользовательский элемент User.Graf

---
+++ Pages/Items/User.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using ChatStudents_Graf.Classes.Common;
+using ChatStudents_Graf.Models;
+
+
+namespace ChatStudents_Graf.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для User.xaml
+    /// </summary>
+    public partial class User : UserControl
+    {
+        Users user;
+        Main main;
+        public User(Users user, Main main)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.main = main;
+            imgUser.Source = BitmapFromArrayByte.LoadImage(user.Photo);
+            FIO.Content = user.ToFIO();
+        }
+        private void SelectChat(object sender, System.Windows.Input.MouseButtonEventArgs e) =>
+            main.SelectUser(user);
+    }
+}
