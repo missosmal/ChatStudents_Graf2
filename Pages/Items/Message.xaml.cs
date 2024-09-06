f7c97bbf88c2ac64a90e93a13c015c7b2a3a9e69
Muxaaaa <20RWplay@gmail.com>
2024-09-06 09:58:31 +0500

    Разработанный пользовательский элемент Message.Graf

---
+++ Pages/Items/Message.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+using ChatStudents_Graf.Classes.Common;
+using ChatStudents_Graf.Models;
+
+namespace ChatStudents_Graf.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Messages.xaml
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message(Messages Messages, Users UserFrom)
+        {
+            InitializeComponent();
+            imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            FIO.Content = UserFrom.ToFIO();
+            tbMessage.Text = Messages.Message;
+        }
+    }
+}
