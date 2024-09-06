41a6f78efb46b3075274f95c04374623926926c5
Muxaaaa <20RWplay@gmail.com>
2024-09-06 09:46:01 +0500

    Разработанные общие классы.Graf

---
+++ Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ChatStudents_Graf.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            BitmapImage Image = new BitmapImage();
+            using (var Stream = new MemoryStream(imageData))
+            {
+                Stream.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.UriSource = null;
+                Image.StreamSource = Stream;
+                Image.EndInit();
+            }
+            Image.Freeze();
+            return Image;
+        }
+    }
+}
---
+++ Classes/Common/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Graf.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=stedert.permaviat.ru;" +
+            "Trusted_Connection = False;" +
+            "Database = ***;" +
+            "User = ***;" +
+            "Pwd = ***;";
+    }
+}
