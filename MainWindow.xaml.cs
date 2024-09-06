ca1bb226c22b6b57477b2a0c5d1970ec84b3429d
Muxaaaa <20RWplay@gmail.com>
2024-09-06 10:01:24 +0500

    Выполненная практическая работа.Graf

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChatStudents_Graf.Models;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChatStudents_Graf
 {
@@ -20,9 +9,15 @@ namespace ChatStudents_Graf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow Instance;
+        public Users LoginUser = null;
         public MainWindow()
         {
             InitializeComponent();
+            Instance = this;
+            OpenPages(new Pages.Login());
         }
+        public void OpenPages(Page page) =>
+            frame.Navigate(page);
     }
 }
