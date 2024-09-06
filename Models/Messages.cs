05685b0633ed9b034f71d052f1eec172a7658ff3
Muxaaaa <20RWplay@gmail.com>
2024-09-06 09:44:26 +0500

    Разработанные модели.Graf

---
+++ Models/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Graf.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public int UserFrom { get; set; }
+        public int UserTo { get; set; }
+        public string Message { get; set; }
+        public Messages(int UserFrom, int UserTo, string Message)
+        {
+            this.UserFrom = UserFrom;
+            this.UserTo = UserTo;
+            this.Message = Message;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Graf.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public byte[] Photo { get; set; }
+        public Users(string Lastname, string Firstname, string Surname, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+    }
+}
